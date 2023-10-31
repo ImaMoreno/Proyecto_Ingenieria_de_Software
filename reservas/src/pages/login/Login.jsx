e37b720823e270178e14d6200dc820a268d47652
Ima_MC <imanolmorenocondori@gmail.com>
2023-10-31 16:27:05 -0500

    ACTUAL 31-OCTUBRE

--- reservas/src/pages/login/Login.jsx
+++ reservas/src/pages/login/Login.jsx
@@ -2,7 +2,8 @@ import { useContext, useState } from "react";
 import "./login.css";
 import { AuthContext } from "../../context/AuthContext";
 import axios from "axios";
-
+import { useNavigate } from "react-router-dom";
+import toast, {Toaster} from "react-hot-toast";
 
 const Login = () => {
     const [credencials,setCredentians] = useState({
@@ -10,39 +11,77 @@ const Login = () => {
         password:undefined,
     });
 
-    const {user,loading,error,dispatch} = useContext(AuthContext);
+    const {user, loading, error, dispatch} = useContext(AuthContext)
+    const navigate = useNavigate()
 
     const handleChange = (e) => {
         setCredentians((prev) =>({...prev,[e.target.id]:e.target.value}));
-    } 
-
-    const login = async (e) =>{
+    }
+    const login = async (e) => {
         e.preventDefault();
+        
         dispatch({type:"LOGIN_START"});
         try{
+            toast.loading("Espere..")
             const res = await axios.post("/auth/login",credencials);
             dispatch({type:"LOGIN_SUCCESS",payload:res.data});
+            toast.dismiss()
+            toast.success("Bienvenid@ al sistema",{
+                duration: 2000,
+                position: 'top-right'
+            })
+            toast.loading("Cargando Propiedades..")
+            
+            setTimeout(()=>{
+                toast.dismiss()
+                navigate("/")
+            },1500)
         }catch(err){
-            dispatch({type:"LOGIN_FAILURE",payload:err.response.data});   
+            toast.dismiss()
+            toast.error(`Error de acceso: ${err.response.data.message}`,
+            {
+                duration: 3000,
+                position: 'top-right'
+            })
+            dispatch({type:"LOGIN_FAILURE",payload:err.response.data})
         }
     }
 
-    console.log(user);
-  
+    console.log(user)
+
     return (
-        <div className="login">
-            <div className="lContainer">
-                <input type="text" placeholder="Nombre de usuario" id="username" className="lInput" 
-                onChange={handleChange}
-                />
-                <input type="text" placeholder="Contraseña" id="password" className="lInput" 
-                onChange={handleChange}
-                />
-                <button className="lButton" onClick={login}>Ingresar</button>
-                {error && <span>{error.message}</span>}
+        <div className="container-login">
+            <div className="main">
+                <input type="checkbox" id="label" arias-hidden="true"/>
+            <div className="login">
+                <label for="label">Iniciar Sesion</label>
+                    <input type="text" placeholder="Nombre de usuario" id="username" className="lInput" 
+                    onChange={handleChange}
+                    />
+                    <input type="password" placeholder="Contraseña" id="password" className="lInput" 
+                    onChange={handleChange}
+                    />
+                    <button className="lButton" onClick={login}>Ingresar</button>
+                    {error && <span>{error.message}</span>}
+            </div>
+            <div className="register">
+            <label for="label">Registro</label>
+                    
+                    <input type="text" placeholder="Nombre de usuario" id="username" className="lInput" 
+                    onChange={handleChange}
+                    />
+                    <input type="password" placeholder="Contraseña" id="password" className="lInput" 
+                    onChange={handleChange}
+                    />
+                    <button className="lButton" onClick={login}>registrar</button>
+                    {error && <span>{error.message}</span>}
+                
             </div>
         </div>
-  )
+        <Toaster/>
+        </div>
+    
+    )
 }
 
 export default Login
